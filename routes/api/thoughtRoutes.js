37401c5b00d72bf1931100934d1e1958676f6953
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2023-12-23 15:55:04 -0700

    add thoughtRouters

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getthoughts,
+  getSinglethought,
+  createthought,
+  deletethought,
+  addreaction,
+  removereaction,
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getthoughts).post(createthought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSinglethought).delete(deletethought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addreaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removereaction);
+
+module.exports = router;
